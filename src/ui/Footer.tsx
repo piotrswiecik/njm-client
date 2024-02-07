6ff2395f1e5a0df5eea940d6632322c3e6899391
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-07 16:58:54 +0100

    basic layout skeleton

---
+++ src/ui/Footer.tsx
@@ -0,0 +1,18 @@
+export const Footer = () => {
+	return (
+		<div className="border-t flex w-full flex-row justify-between p-4">
+			<span className="text-xs text-slate-500">
+				NJM Webstore © 2024{" "}
+				<a
+					className="hover:text-slate-700"
+					href="mailto:piotr.swiecik@gmail.com"
+				>
+					piotr.swiecik@gmail.com
+				</a>
+			</span>
+			<span className="text-xs text-slate-500">
+				Powered by Next.js & Node.js
+			</span>
+		</div>
+	);
+};
