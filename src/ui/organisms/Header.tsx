2a80fd25d918ea3c0d338072d98737a03ac0204d
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-15 12:18:59 +0100

    responsive header

--- src/ui/organisms/Header.tsx
+++ src/ui/organisms/Header.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export const Header = () => {
 	return (
-		<div className="flex w-full flex-row p-4">
-			<h1 className="text-2xl font-bold text-slate-800">NJM RECORD STORE</h1>
+		<div className="flex w-full flex-col items-center p-4 sm:flex-row sm:items-baseline">
+			<h1 className="text-2xl font-bold text-slate-800 mb-6 sm:mb-0">NJM RECORD STORE</h1>
+			<nav className="mx-8">
+				<Link href="#" className="px-4 text-slate-800 hover:text-slate-500">
+					Home
+				</Link>
+				<Link href="#" className="px-4 text-slate-800 hover:text-slate-500">
+					All
+				</Link>
+			</nav>
 		</div>
 	);
 };
