903107eb7eaf91c2841a666e5b3ace7a5b038b2e
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-17 17:54:49 +0100

    cart hover effect

--- src/ui/atoms/ShoppingCartLink.tsx
+++ src/ui/atoms/ShoppingCartLink.tsx
@@ -3,9 +3,14 @@ import Link from "next/link";
 
 const ShoppingCartLink = () => {
 	return (
-		<div className="m-0 hidden flex-row items-baseline p-0 sm:flex">
+		<div className="m-0 hidden flex-row items-baseline p-0 hover:opacity-75 sm:flex transition-opacity duration-300">
 			<Link href="#">
-				<ShoppingCart color="#334155" strokeWidth={2} size={36} className="inline-block" />
+				<ShoppingCart
+					color="#334155"
+					strokeWidth={2}
+					size={36}
+					className="inline-block"
+				/>
 			</Link>
 		</div>
 	);
