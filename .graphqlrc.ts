9bb41f646f27b3404d50a6430a707726d6a04c56
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-19 16:52:30 +0100

    codegen config adjusted

--- .graphqlrc.ts
+++ .graphqlrc.ts
@@ -3,12 +3,22 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "./graphql/",
-  documents: "src/**/*.tsx",
+  schema: "./graphql/*.graphql",
+  documents: "src/graphql/*.graphql",
+  ignoreNoDocuments: true,
   generates: {
     "src/gql/": {
       preset: "client",
-      plugins: []
+      plugins: [],
+      presetConfig: {
+        fragmentMasking: false
+      },
+      config: {
+        defaultScalarType: "unknown",
+        useTypeImports: true,
+        skipTypename: true,
+        documentMode: "string"
+      }
     }
   }
 };
