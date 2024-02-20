1432bf80c7034a51485db78e8e6acee2b4907096
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-20 14:51:24 +0100

    refactoring product details view

--- src/ui/atoms/ProductDetailsInfo.tsx
+++ src/ui/atoms/ProductDetailsInfo.tsx
@@ -10,45 +10,96 @@ const ProductInfoPanel = ({ product }: ProductDetailsProps) => {
 	console.log(product);
 	return (
 		<>
-			<div className="border-b border-slate-400 pb-2">
+			<header className="border-b border-slate-400 pb-2">
 				<h1 className="text-lg font-semibold">{product.title}</h1>
-				<div className="italic text-slate-600">{product.artist}</div>
-			</div>
-			<table className="table-auto mt-6">
+				<p className="italic text-slate-600">{product.artist}</p>
+			</header>
+			<section className="my-4">
+				<table className="border-separate border-spacing-y-2">
+					<tbody>
+						<tr>
+							<td>Category:</td>
+							<td className="capitalize italic text-slate-600">
+								{product.category}
+							</td>
+						</tr>
+						<tr>
+							<td>Released:</td>
+							{/* todo util to reformat date to year */}
+							<td className="italic text-slate-600">
+								{product.releaseDate.slice(0, 4)}
+							</td>
+						</tr>
+						<tr>
+							<td>Tracks:</td>
+							<td className="italic text-slate-600">{product.tracks.length}</td>
+						</tr>
+					</tbody>
+				</table>
+				<div className="my-4">
+					Select format:
+					<div className="flex flex-row py-2">
+						<button
+							type="button"
+							className="mb-2 me-2 rounded-lg border border-gray-800 px-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
+						>
+							CD
+						</button>
+						<button
+							type="button"
+							className="mb-2 me-2 rounded-lg border border-gray-800 px-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
+						>
+							LP
+						</button>
+					</div>
+					<div className="flex flex-row items-baseline justify-between">
+						<div className="text-xl">{formatPrice(product.price)}</div>
+						<button
+							type="button"
+							className="mb-2 me-2 rounded-lg border border-gray-800 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
+						>
+							Add to cart
+						</button>
+					</div>
+				</div>
+			</section>
+			{/* <table className="mt-6 table-auto border-separate">
 				<tbody>
-					
-					
 					<tr>
 						<td className="font-semibold">Category:</td>
-						<td className="capitalize italic text-slate-600">{product.category}</td>
+						<td className="capitalize italic text-slate-600">
+							{product.category}
+						</td>
 					</tr>
 					<tr>
 						<td className="font-semibold">Released:</td>
-						<td className="capitalize italic text-slate-600">{product.releaseDate.slice(0,4)}</td>
+						<td className="capitalize italic text-slate-600">
+							{product.releaseDate.slice(0, 4)}
+						</td>
 					</tr>
-				</tbody>
-
-				
-
-			</table>
-<div className="flex flex-row mt-4">
-					<div className="font-semibold">
-
-					
-					Format:</div>
-					<button
+					<tr>
+						<td className="font-semibold">Format:</td>
+						<td>
+						<button
 					type="button"
-					className="mb-2 me-2 px-2 rounded-lg border border-gray-800 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
+					className="mb-2 me-2 rounded-lg border border-gray-800 px-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
 				>
 					CD
 				</button>
 				<button
 					type="button"
-					className="mb-2 me-2 px-2 rounded-lg border border-gray-800 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
+					className="mb-2 me-2 rounded-lg border border-gray-800 px-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
 				>
 					LP
 				</button>
-					</div>
+						</td>
+					</tr>
+				</tbody>
+			</table> */}
+			{/* <div className="mt-4 flex flex-row">
+				<span className="font-semibold mr-4">Format:</span>
+				
+			</div> */}
 			{/* <div className="">
 				<div className="mb-2">Tracks:</div>
 				<ol className="list-decimal p-4 text-xs">
@@ -57,15 +108,6 @@ const ProductInfoPanel = ({ product }: ProductDetailsProps) => {
 					))}
 				</ol>
 			</div> */}
-			<div className="flex flex-row items-baseline justify-between">
-				<div className="text-xl">{formatPrice(product.price)}</div>
-				<button
-					type="button"
-					className="mb-2 me-2 rounded-lg border border-gray-800 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800"
-				>
-					Add to cart
-				</button>
-			</div>
 		</>
 	);
 };
--- src/ui/components/ProductDetails.tsx
+++ src/ui/components/ProductDetails.tsx
@@ -4,17 +4,13 @@ import ProductImage from "@/ui/atoms/ProductImage";
 
 const ProductDetails = async ({ product }: { product: ProductDetailsDto }) => {
 	return (
-		<div className="mx-auto max-w-7xl px-6 sm:px-12">
-			<div className="flex flex-col sm:flex-row justify-start">
-				<div className="sm:max-h-96 sm:max-w-96 m-0 sm:mr-8">
-					<ProductImage url={product.image.url} alt={"temp"} />
-				</div>
-				<div className="m-8 flex flex-col sm:m-0 sm:w-5/12">
-					<div className="my-2">
-						<ProductInfoPanel product={product} />
-					</div>
-				</div>
+		<div className="mx-auto flex max-w-7xl flex-col justify-start px-6 sm:flex-row sm:px-12">
+			<div className="m-0 sm:mr-8">
+				<ProductImage url={product.image.url} alt={"temp"} />
 			</div>
+			<section className="m-8 flex flex-col sm:m-0 sm:w-5/12">
+					<ProductInfoPanel product={product} />
+			</section>
 		</div>
 	);
 };
