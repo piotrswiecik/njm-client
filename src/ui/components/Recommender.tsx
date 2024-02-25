af7089fda978ff1cbfe2e9dcfe6de2f4d4a481ba
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-25 16:17:00 +0100

    adjust recommender component

--- src/ui/components/Recommender.tsx
+++ src/ui/components/Recommender.tsx
@@ -10,19 +10,25 @@ type RecommenderComponentProps = {
 };
 
 const RecommenderComponent = async ({
-	categoryName, leadText,
+	categoryName,
+	leadText,
 }: RecommenderComponentProps) => {
 	// currently returns N products from same category
 	const recommendedProducts: ProductOverviewFragment[] =
 		await getRecommendation({ categoryName: categoryName }, 4);
 	return (
-		<section data-testid="related-products">
-			<h2 className="xl:text-xl font-bold">{leadText ? leadText : "You might also like:"}</h2>
-			<ul className="flex flex-col flex-wrap justify-evenly py-4 sm:flex-row">
+		<section
+			data-testid="related-products"
+			className="sm:max-w-xl lg:max-w-5xl"
+		>
+			<h2 className="mb-2 font-bold xl:text-xl">
+				{leadText ? leadText : "You might also like:"}
+			</h2>
+			<ul className="grid grid-cols-1 grid-rows-4 sm:max-w-[300] sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-4 lg:grid-rows-1">
 				{recommendedProducts.map((product) => (
 					<li
 						key={product.id}
-						className="group relative max-w-[200px] list-none"
+						className="group relative inline-block max-w-[200px] list-none pr-2"
 					>
 						<Link href={`/product/${product.id}`}>
 							<RecommenderItem product={product} />
