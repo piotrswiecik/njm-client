fc49a2d1d575eaf3dda2fe3d13892d9cefe15e23
Piotr Święcik <piotr.swiecik@gmail.com>
2024-02-25 18:18:32 +0100

    refactor product details server component form

--- src/ui/atoms/ProductDetailsInfo.tsx
+++ src/ui/atoms/ProductDetailsInfo.tsx
@@ -7,18 +7,41 @@ type ProductDetailsProps = {
 
 // TODO maybe refactor into subcomponents - atoms
 const ProductInfoPanel = ({ product }: ProductDetailsProps) => {
-	const inStock = (product: ProductDetailsFragment, type: "cd" | "lp") => {
+	const variantInStock = (
+		product: ProductDetailsFragment,
+		type: "cd" | "lp",
+	) => {
 		const variant = product.variants.find((v) => v.name === type);
 		if (!variant) return false;
 		return variant.stock > 0;
 	};
 
-	console.log(product.variants);
+	const productAvailable = (product: ProductDetailsFragment) => {
+		console.log(`cd qty: ${product.variants[0].stock}`);
+		console.log(`lp qty: ${product.variants[1].stock}`);
+		return product.variants.some((v) => v.stock > 0);
+	};
 
-	const variantEnabledClassName = `mb-2 me-2 rounded-lg border border-gray-800 px-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800`;
+	const defaultVariant = (product: ProductDetailsFragment) => {
+		if (!productAvailable(product)) return null;
+		if (!variantInStock(product, "cd")) return "lp";
+		if (!variantInStock(product, "lp")) return "cd";
+		return "cd";
+	}
+
+	const addItemToCart = async (x: FormData) => {
+		"use server";
+		// we need item id
+		const id = product.id;
+		console.log(product.id);
+		// and we need selected variant info
+		console.log(x);
+	};
+
+	const variantEnabledClassName = `hover:bg-slate-300 cursor-pointer peer-checked:text-red-500 mb-2 me-2 rounded-lg border border-gray-800 px-2 text-center text-sm font-medium text-gray-900 peer-checked:bg-gray-900 peer-checked:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800`;
 	const variantDisabledClassName = `mb-2 me-2 rounded-lg border border-gray-400 px-2 text-center text-sm font-medium text-gray-400 dark:border-gray-600 dark:text-gray-400`;
 	const inStockClassName = `mb-2 me-2 rounded-lg border border-gray-800 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800`;
-	const outOfStockClassName = `mb-2 me-2 rounded-lg border border-gray-400 px-5 py-2.5 text-center text-sm font-medium text-gray-400 dark:border-gray-600 dark:text-gray-400`;
+	const outOfStockClassName = `mb-2 me-2 cursor-default rounded-lg border border-gray-400 px-5 py-2.5 text-center text-sm font-medium text-gray-400 dark:border-gray-600 dark:text-gray-400`;
 
 	return (
 		<>
@@ -48,45 +71,72 @@ const ProductInfoPanel = ({ product }: ProductDetailsProps) => {
 						</tr>
 					</tbody>
 				</table>
-				<div className="my-4">
-					Select format:
-					<div className="flex flex-row py-2">
-						<button
-							type="button"
-							disabled={!inStock(product, "cd")}
-							className={
-								!inStock(product, "cd")
-									? variantDisabledClassName
-									: variantEnabledClassName
-							}
-						>
-							CD
-						</button>
-						<button
-							type="button"
-							disabled={!inStock(product, "lp")}
-							className={
-								!inStock(product, "cd")
-									? variantDisabledClassName
-									: variantEnabledClassName
-							}
-						>
-							LP
-						</button>
-					</div>
-					<div className="flex flex-row items-baseline justify-between">
-						<div className="text-xl">
-							{formatPrice(getBasicVariantPrice(product))}
+
+				{/* TODO: probably a good idea to create a separate component for this */}
+				<form action={addItemToCart}>
+					<div className="my-4">
+						Select format:
+						<ul className="flex flex-row py-2">
+							<li>
+								<input
+									id="variant-cd"
+									name="variant"
+									type="radio"
+									value="cd"
+									defaultChecked={defaultVariant(product) === "cd"}
+									disabled={!variantInStock(product, "cd")}
+									className={`peer hidden`}
+								/>
+								<label
+									htmlFor="variant-cd"
+									className={
+										!variantInStock(product, "cd")
+											? variantDisabledClassName
+											: variantEnabledClassName
+									}
+								>
+									CD
+								</label>
+							</li>
+							<li>
+								<input
+									id="variant-lp"
+									name="variant"
+									type="radio"
+									value="lp"
+									defaultChecked={defaultVariant(product) === "lp"}
+									disabled={!variantInStock(product, "lp")}
+									className={`peer hidden`}
+								/>
+								<label
+									htmlFor="variant-lp"
+									className={
+										!variantInStock(product, "lp")
+											? variantDisabledClassName
+											: variantEnabledClassName
+									}
+								>
+									LP
+								</label>
+							</li>
+						</ul>
+						<div className="flex flex-row items-baseline justify-between">
+							<div className="text-xl">
+								{formatPrice(getBasicVariantPrice(product))}
+							</div>
+
+							<button
+								type="submit"
+								disabled={!productAvailable(product)}
+								className={
+									productAvailable(product) ? inStockClassName : outOfStockClassName
+								}
+							>
+								{productAvailable(product) ? "Add to cart" : "Out of stock"}
+							</button>
 						</div>
-						<button
-							type="button"
-							disabled={!inStock(product, "lp")}
-							className={!inStock(product, "cd") ? outOfStockClassName : inStockClassName}
-						>
-							{!inStock(product, "cd") ? "Out of stock" : "Add to cart"}
-						</button>
 					</div>
-				</div>
+				</form>
 			</section>
 		</>
 	);
