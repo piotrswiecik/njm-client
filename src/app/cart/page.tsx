f35527b54b4286ab3e2e3ce81788548e2b1e2ef3
Piotr Święcik <piotr.swiecik@gmail.com>
2024-03-02 20:24:36 +0100

    add total to cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,4 +1,5 @@
 import { getCart } from "@/api/cart";
+import { formatPrice } from "@/lib/utils";
 import CartItemCard from "@/ui/molecules/CartItemCard";
 
 const CartPage = async () => {
@@ -6,11 +7,15 @@ const CartPage = async () => {
 	if (!cart) {
 		return <>empty cart page</>;
 	}
-	console.log(cart);
 
 	if (!cart.orderItems || cart.orderItems.length === 0) {
 		return <>empty cart page</>;
 	}
+
+	const total = cart.orderItems.reduce(
+		(acc, item) => acc + item.quantity * item.variant.price,
+		0,
+	);
 
 	return (
 		<>
@@ -24,6 +29,12 @@ const CartPage = async () => {
 						<CartItemCard key={item.variant.id} item={item} cartId={cart.id} />
 					);
 				})}
+			<div className="flex flex-row justify-end">
+				<div className="px-4">
+					<span className="text-lg font-bold px-1">Total: </span>
+					<span>{formatPrice(total)}</span>
+				</div>
+			</div>
 		</>
 	);
 };
