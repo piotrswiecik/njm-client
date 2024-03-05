d240c34a01d850c316f87b33b9ccda175cf4ed4a
Piotr Święcik <piotr.swiecik@gmail.com>
2024-03-05 19:59:06 +0100

    add clerk provider to app root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Header } from "@/ui/organisms/Header";
 import { Footer } from "@/ui/organisms/Footer";
 
@@ -17,18 +18,20 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={`${inter.className} bg-slate-200`}>
-				<div className="flex min-h-screen flex-col">
-					<div className="grow-0">
-						<Header />
+		<ClerkProvider>
+			<html lang="en">
+				<body className={`${inter.className} bg-slate-200`}>
+					<div className="flex min-h-screen flex-col">
+						<div className="grow-0">
+							<Header />
+						</div>
+						<main className="grow sm:mt-4">{children}</main>
+						<footer className="grow-0">
+							<Footer />
+						</footer>
 					</div>
-					<main className="grow sm:mt-4">{children}</main>
-					<footer className="grow-0">
-						<Footer />
-					</footer>
-				</div>
-			</body>
-		</html>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
