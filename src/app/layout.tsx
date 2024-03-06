0c65fcdcbfd952076f48238e57fc56ca98204bab
Piotr Święcik <piotr.swiecik@gmail.com>
2024-03-06 20:01:22 +0100

    change global font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import { Inter, Raleway, Hind, Work_Sans, Lato, Nunito, Cantarell } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Header } from "@/ui/organisms/Header";
 import { Footer } from "@/ui/organisms/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
-const raleway = Raleway({ subsets: ["latin"] });
-const workSans = Work_Sans({ subsets: ["latin"] });
-const hind = Hind({ subsets: ["latin"], weight: ["300", "400", "500", '600', "700"]});
+//const inter = Inter({ subsets: ["latin"] });
 const lato = Lato({ subsets: ["latin"], weight: ["300", "400", "700"] });
-const nunito = Nunito({ subsets: ["latin"], weight: ["300", "400", "700"] });
-const cantarell = Cantarell({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
 	title: "NJM Store",
@@ -26,7 +21,7 @@ export default function RootLayout({
 	return (
 		<ClerkProvider>
 			<html lang="en">
-				<body className={`${inter.className} bg-slate-200`}>
+				<body className={`${lato.className} bg-slate-200`}>
 					<div className="flex min-h-screen flex-col">
 						<div className="grow-0">
 							<Header />
