4317798b3492e7b841d44df4029fe8509f8ce848
Piotr Święcik <piotr.swiecik@gmail.com>
2024-03-07 16:12:33 +0100

    turn off searchbar autocomplete

--- src/ui/molecules/SearchBar.tsx
+++ src/ui/molecules/SearchBar.tsx
@@ -42,6 +42,7 @@ const SearchBar = () => {
 					type="text"
 					name="search"
 					id="search"
+					autoComplete="off"
 					value={term ?? ""}
 					className="block w-60 flex-1 rounded-lg border-0 py-1.5 pl-2 text-gray-900 outline-none placeholder:text-gray-400 focus-within:ring-2 focus-within:ring-inset focus-within:ring-slate-400 sm:text-sm sm:leading-6"
 					placeholder="Search"
