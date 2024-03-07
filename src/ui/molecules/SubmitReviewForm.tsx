c3d4771b69f50d4883eaecb307f8dff3455d7adc
Piotr Święcik <piotr.swiecik@gmail.com>
2024-03-07 09:10:08 +0100

    fix rating selector styling

--- src/ui/molecules/SubmitReviewForm.tsx
+++ src/ui/molecules/SubmitReviewForm.tsx
@@ -13,7 +13,7 @@ const SubmitReviewForm = ({
 	active: boolean;
 }) => {
 	const activeStarClassName = "text-yellow-300";
-	const inactiveStarClassName = "text-gray-300 dark:text-gray-500";
+	const inactiveStarClassName = "text-slate-100";
 	const baseStarClassName = "ms-1 h-4 w-4";
 	// TODO: refactor out
 	const activeButtonClassName = `mb-2 me-2 rounded-lg border border-gray-800 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-800`;
