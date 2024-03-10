f5ad664dcc7df1ba5c1dee037d72fdd8f77c9610
Piotr Święcik <piotr.swiecik@gmail.com>
2024-03-10 19:37:11 +0100

    fixed input role for e2e

--- src/ui/molecules/SearchBar.tsx
+++ src/ui/molecules/SearchBar.tsx
@@ -36,12 +36,13 @@ const SearchBar = () => {
 	};
 
 	return (
-		<form onSubmit={(e) => handleSearch(e)} role="searchbox">
+		<form onSubmit={(e) => handleSearch(e)}>
 			<div className="relative flex rounded-lg shadow-sm sm:max-w-md">
 				<input
 					type="text"
 					name="search"
 					id="search"
+					role="searchbox"
 					autoComplete="off"
 					value={term ?? ""}
 					className="block w-60 flex-1 rounded-lg border-0 py-1.5 pl-2 text-gray-900 outline-none placeholder:text-gray-400 focus-within:ring-2 focus-within:ring-inset focus-within:ring-slate-400 sm:text-sm sm:leading-6"
